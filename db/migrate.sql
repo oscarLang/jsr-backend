b7f79fbf8f92974abe059a5931be7a9d5a80ebc0
Oscar Lang <oscar.lang98@gmail.com>
2020-09-15 08:51:04 +0100

    Filled readme and added it to week/2

--- db/migrate.sql
+++ db/migrate.sql
@@ -14,14 +14,31 @@ CREATE TABLE IF NOT EXISTS reports (
 );
 
 INSERT INTO reports (week, text_data) VALUES (1, "## Available Scripts
-    In the project directory, you can run:
+In the project directory, you can run:
 
-    ### `npm install`
-    Install all the dependencies of the project.
+### `npm install`
+Install all the dependencies of the project.
 
-    ### `npm start`
-    Runs the app in the development mode.Open [http://localhost:3000](http://localhost:3000) to view it in the browser. The page will reload if you make edits. You will also see any lint errors in the console.
+### `npm start`
+Runs the app in the development mode.Open [http://localhost:3000](http://localhost:3000) to view it in the browser. The page will reload if you make edits. You will also see any lint errors in the console.
 
-    ### `npm build`
-    Builds the app for production to the `build` folder.
-    It correctly bundles React in production mode and optimizes the build for the best performance. The build is minified and the filenames include the hashes. Your app is ready to be deployed!");
+### `npm build`
+Builds the app for production to the `build` folder.
+It correctly bundles React in production mode and optimizes the build for the best performance. The build is minified and the filenames include the hashes. Your app is ready to be deployed!");
+
+
+INSERT INTO reports (week, text_data) VALUES (2, "# Available Scripts
+In the project directory, you can run:
+
+### `npm install`
+Install all the dependencies of the project.
+
+### `npm start`
+
+Runs the server in the development mode.
+
+Open [http://localhost:1337](http://localhost:1337) to view it in the browser.
+
+The server will restart if you make edits.
+
+You will also see any lint errors in the console.");
